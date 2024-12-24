a4fe799feb425b0e8492c07f45358b0da2e7e8bf
sudharshanrajaram <sudharshanrajaram@gmail.com>
2024-12-24 13:58:23 +0530

    changes

--- Video-Call/server.js
+++ Video-Call/server.js
@@ -6,16 +6,12 @@ require("dotenv").config();
 const { Server } = require("socket.io");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-connectDB();
-
-PORT = 8000;
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
-const io = new Server(server, {
-  cors: true,
+const http = require("http");
+const server = http.createServer(app);
+const io = new Server(process.env.PORT || 5000, {
+  cors: {
+    origin: "*",
+  },
 });
 
 const emailToSocketIdMap = new Map();
